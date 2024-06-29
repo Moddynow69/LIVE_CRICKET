09a7e1a1a8bbdcf00799b923ac6b1aee5a537cbe
Tushar <tusharkaler450@gmail.com>
2024-06-29 22:46:36 +0530

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen">
+      <iframe
+        src="//stream.crichd.vip/update/star1hi.php"
+        width="100%"
+        height="500px"
+        marginheight="0"
+        marginwidth="0"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+}
