4bd40a549512a3e37d4e68c062b421b29caddf46
Tushar <tusharkaler450@gmail.com>
2025-01-03 13:47:22 +0530

    any link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,40 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [language, setLanguage] = useState("English");
+  const [source, setSource] = useState("");
+  const [value, setValue] = useState("");
   return (
     <div className="w-screen h-screen flex justify-center flex-col">
-      <div className="w-screen h-20  flex justify-center flex-row gap-10">
-        <div className="w-40 h-auto flex justify-center items-center text-4xl hover:text-[40px] hover:text-[#ffffff97] cursor-pointer" onClick={()=>setLanguage("English")}>English</div>
-        <div className="w-40 h-auto flex justify-center items-center text-4xl hover:text-[40px] hover:text-[#ffffff97] cursor-pointer" onClick={()=>setLanguage("Hondi")}>Hindi</div>
-      </div>
-      <iframe
-        src={
-          language === "English"
-            ? "//stream.crichd.vip/update/skys2.php"
-            : "//stream.crichd.vip/update/star1hi.php"
-        }
-        width="100%"
-        height="100%"
-        allow="encrypted-media"
-        allowFullScreen
-      ></iframe>
+      <form className="w-screen h-16 p-2  flex justify-center items-center flex-row gap-10">
+        <input
+          className="w-1/5 h-full rounded text-black px-2"
+          placeholder="//stream.crichd.sc/update/star1hi.php"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="w-fit px-3 rounded h-full bg-slate-500"
+          onClick={(e) => {
+            e.preventDefault();
+            setSource(value);
+          }}
+        >
+          Submit
+        </button>
+      </form>
+      {source === "" ? (
+        <div className="h-full animate-pulse">Waiting..........</div>
+      ) : (
+        <iframe
+          className="border-white border-2"
+          src={source}
+          width="100%"
+          height="100%"
+          allow="encrypted-media"
+          allowFullScreen
+        ></iframe>
+      )}
     </div>
   );
 }
